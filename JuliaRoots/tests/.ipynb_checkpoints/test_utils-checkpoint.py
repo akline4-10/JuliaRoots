8ecaa523d55836fb440aea62f163e7c2ef58282e
Aaron <apkline1@gmail.com>
2018-04-03 14:13:16 -0600

    Files to translate

---
+++ JuliaRoots/tests/.ipynb_checkpoints/test_utils-checkpoint.py
@@ -0,0 +1,189 @@
+import pytest
+import numpy as np
+import sympy as sy
+from numalgsolve import utils as ut
+from numalgsolve.utils import *
+from numalgsolve.polynomial import MultiCheb, MultiPower
+from scipy.linalg import qr, solve_triangular
+
+def test_inverse_P():
+    # Test Case 1:
+    # Create matrix
+    M = np.array([[0, 1, 2, 3, 4, 5],
+       [0, 1, 2, 3, 4, 5],
+       [0, 1, 2, 3, 4, 5],
+       [0, 1, 2, 3, 4, 5]])
+
+    # Order of Column flips.
+    p = [1,4,3,2,0,5]
+
+    # N is the matrix with the columns flipped.
+    N = M[:,p]
+
+    # Find the order of columns to flip back to.
+    pt = inverse_P(p)
+    print(pt)
+    # Result done by hand.
+    pt_inv = [4,0,3,2,1,5]
+    assert(np.allclose(M,N[:,pt])), "Matrix are not the same."
+    assert(np.all(pt == pt_inv)), "Wrong matrix order."
+
+
+    # Test Case 2:
+    A = np.random.random((5,10))
+
+    Q,R,P = qr(A,pivoting=True)
+
+    pt = inverse_P(P)
+
+    # We know that A[:,p] = QR, want to see if pt flips QR back to A.
+    assert(np.allclose(A,(Q@R)[:,pt]))
+
+def test_triangular_solve():
+    """This tests the triangular_solve() method.
+    A visual graph of zeroes on the diagonal was also used to test this function.
+    """
+    # Simple Test Case.
+    A = np.array([[1, 2, 3, 4, 5],
+                  [0, 1, 2, 3, 4],
+                  [0, 0, 0, 1, 2]])
+
+    matrix = triangular_solve(A)
+    answer = np.array([[ 1.,  0., -1.,  0.,  1.],
+                       [ 0.,  1.,  2.,  0., -2.],
+                       [ 0.,  0.,  0.,  1.,  2.]])
+    assert(np.allclose(matrix,answer))
+
+    # Randomize test case: square matrix.
+    A = np.random.random((50,50))
+    Q,R,p = qr(A,pivoting=True)
+    diagonal = np.diag(R)
+    r = np.sum(np.abs(diagonal)>1e-10)
+    matrix = R[:r]
+    new_matrix = triangular_solve(matrix)
+    true=sy.Matrix(new_matrix).rref()
+    x = sy.symbols('x')
+    f = sy.lambdify(x,true[0])
+    assert(np.allclose(new_matrix,f(1)))
+
+    # Randomize test case: shorter rows than column.
+    A = np.random.random((10,50))
+    Q,R,p = qr(A,pivoting=True)
+    diagonal = np.diag(R)
+    r = np.sum(np.abs(diagonal)>1e-10)
+    matrix = R[:r]
+    new_matrix = triangular_solve(matrix)
+    true=sy.Matrix(new_matrix).rref()
+    x = sy.symbols('x')
+    f = sy.lambdify(x,true[0])
+    print(f(1))
+    print(new_matrix)
+    assert(np.allclose(new_matrix,f(1)))
+
+    # Randomize test case: longer rows than columns.
+    A = np.random.random((50,10))
+    Q,R,p = qr(A,pivoting=True)
+    diagonal = np.diag(R)
+    r = np.sum(np.abs(diagonal)>1e-10)
+    matrix = R[:r]
+    new_matrix = triangular_solve(matrix)
+    true=sy.Matrix(new_matrix).rref()
+    x = sy.symbols('x')
+    f = sy.lambdify(x,true[0])
+    print(f(1))
+    print(new_matrix)
+    assert(np.allclose(new_matrix,f(1)))
+
+def test_sorted_polys_monomial():
+    A = MultiPower(np.array([[3,0,-5,0,0,4,2,-6,3,-6],
+                         [-2,0,-1,1,-1,4,2,-6,-5,-2],
+                         [-1,3,2,-2,0,4,-1,-2,-4,6],
+                         [4,2,5,9,0,3,2,-1,-3,-3],
+                         [3,-3,-5,-2,0,4,-2,2,1,-6]]))
+    x = sorted_polys_monomial([A])
+    assert(A == x[0])
+
+    B = MultiPower(np.array([[2,0,-3,0,0],
+                         [0,1,0,0,0],
+                         [-2,0,0,0,0],
+                         [0,0,4,0,0],
+                         [0,0,0,0,-2]]))
+    assert(list((B,A)) == sorted_polys_monomial([B,A]))
+
+    C = MultiPower(np.array([[0,0,-3,0,0],
+                         [0,0,0,0,0],
+                         [0,0,0,0,0],
+                         [0,0,4,0,0],
+                         [0,0,0,0,-2]]))
+    assert(list((C,B,A)) == sorted_polys_monomial([A,B,C]))
+
+
+    D = MultiPower(np.array([[2,0,-3,0,0],
+                         [0,1,0,0,0],
+                         [-2,0,2,0,0],
+                         [0,0,4,0,0],
+                         [0,0,0,0,-2]]))
+    assert(list((C,B,D,A)) == sorted_polys_monomial([B,D,C,A]))
+
+    E = MultiPower(np.array([[3,0,-5,0,0,4,2,-6,3,-6],
+                         [-2,0,-1,1,-1,4,2,-6,-5,-2],
+                         [-1,3,2,-2,0,4,-1,-2,-4,6],
+                         [4,2,5,9,0,3,2,-1,-3,-3],
+                         [3,-3,-5,-2,2,4,-2,2,1,-6]]))
+    assert(list((C,B,D,A,E)) == sorted_polys_monomial([B,D,E,C,A]))
+
+    F = MultiPower(np.array([[3,0,-5,0,0,0,2,-6,3,-6],
+                         [-2,0,-1,1,-1,4,2,-6,-5,-2],
+                         [-1,3,2,-2,0,4,-1,-2,-4,6],
+                         [4,2,5,9,0,3,2,-1,-3,-3],
+                         [3,-3,-5,-2,0,4,-2,2,1,-6]]))
+    assert(list((C,B,D,F,A,E)) == sorted_polys_monomial([F,B,D,E,C,A]))
+
+def test_sorted_polys_coeff():
+    A = MultiPower(np.array([[2,0,-3,0,0],
+                         [0,1,0,0,0],
+                         [-2,0,0,0,0],
+                         [0,0,4,0,0],
+                         [0,0,0,0,-2]]))
+    B = MultiPower(np.array([[3,0,-5,0,0,4,2,-6,3,-6],
+                         [-2,0,-1,1,-1,4,2,-6,-5,-2],
+                         [-1,3,2,-2,0,4,-1,-2,-4,6],
+                         [4,2,5,9,0,3,2,-1,-3,-3],
+                         [3,-3,-5,-2,0,4,-2,2,1,-6]]))
+
+    assert([A,B] == ut.sorted_polys_coeff([A,B]))
+
+    C = MultiPower(np.array([[1]]))
+    assert([C,A,B] == ut.sorted_polys_coeff([A,B,C]))
+
+def test_makePolyCoeffMatrix():
+    A = MultiPower('1')
+    B = MultiPower(np.array([1]))
+    assert (A.coeff==B.coeff).all()
+
+    A = MultiPower('2x0+x1+x0*x1')
+    B = MultiPower(np.array([[0,2],[1,1]]))
+    assert (A.coeff==B.coeff).all()
+
+    A = MultiPower('-4.7x0*x1+2x1+5x0+-3')
+    B = MultiPower(np.array([[-3,5],[2,-4.7]]))
+    assert (A.coeff==B.coeff).all()
+
+    A = MultiPower('x0^2+-x1^2')
+    B = MultiPower(np.array([[0,0,1],[0,0,0],[-1,0,0]]))
+    assert (A.coeff==B.coeff).all()
+
+    A = MultiPower('x0+2x1+3x2+4x3')
+    B = MultiPower(np.array(
+        [[[[0,1],[2,0]],
+          [[3,0],[0,0]]],
+         [[[4,0],[0,0]],
+          [[0,0],[0,0]]]]))
+    assert (A.coeff==B.coeff).all()
+
+    A = MultiPower('1+x0')
+    B = MultiPower('1+x1')
+    A1 = MultiPower(np.array([1,1]))
+    B1 = MultiPower(np.array([[1],[1]]))
+
+    assert (A.coeff==A1.coeff).all() and (B.coeff==B1.coeff).all()
---
+++ JuliaRoots/tests/test_power.py
@@ -0,0 +1,102 @@
+import numpy as np
+import os,sys
+from numalgsolve.polynomial import MultiPower
+import pytest
+import random
+from numalgsolve import utils
+
+
+def test_add():
+    a1 = np.arange(27).reshape((3,3,3))
+    Test2 = MultiPower(a1)
+    a2 = np.ones((3,3,3))
+    Test3 = MultiPower(a2)
+    addTest = Test2 + Test3
+    assert (addTest.coeff == (Test2.coeff + Test3.coeff)).all()
+
+def test_mult():
+    test1 = np.array([[0,1],[2,1]])
+    test2 = np.array([[2,2],[3,0]])
+    p1 = MultiPower(test1)
+    p2 = MultiPower(test2)
+    new_poly = p1*p2
+    truth = MultiPower(np.array([[0, 2, 2],[4,9,2],[6,3,0]]))
+    assert np.allclose(new_poly.coeff, truth.coeff)
+
+def test_mon_mult():
+    """
+    Tests monomial multiplication using normal polynomial multiplication.
+    """
+
+    mon = (1,2)
+    Poly = MultiPower(np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]))
+    mon_matr = MultiPower(np.array([[0,0,0,0],[0,0,1,0],[0,0,0,0],[0,0,0,0]]))
+    P1 = mon_matr*Poly
+    P2 = MultiPower.mon_mult(Poly, mon)
+
+    mon2 = (0,1,1)
+    Poly2 = MultiPower(np.arange(1,9).reshape(2,2,2))
+    mon_matr2 = MultiPower(np.array([[[0,0],[0,1]],[[0,0],[0,0]]]))
+    T1 = mon_matr2*Poly2
+    T2 = MultiPower.mon_mult(Poly2, mon2)
+
+
+    assert np.allclose(P1.coeff, P2.coeff, atol = 1.0e-10)
+    assert np.allclose(T1.coeff, T2.coeff, atol = 1.0e-10)
+
+def test_mon_mult_random():
+    #test with random matrices
+    possible_dim = np.random.randint(1,5, (1,10))
+    dim = possible_dim[0, random.randint(1,9)]
+
+    shape = list()
+    for i in range(dim):
+        shape.append(random.randint(2,4))
+    matrix1 = np.random.randint(1,11,(shape))
+    M1 = MultiPower(matrix1)
+
+    shape2 = list()
+    for i in range(dim):
+        shape2.append(random.randint(2,4))
+    matrix2 = np.ones(shape2)
+    M2 = MultiPower(matrix2)
+
+    M3 = M1*M2
+
+    for index, i in np.ndenumerate(M2.coeff):
+        if sum(index) == 0:
+            M4 = MultiPower.mon_mult(M1, index)
+        else:
+            M4 = M4 + MultiPower.mon_mult(M1, index)
+
+    if M3.shape != M4.shape:
+        new_M3_coeff, new_M4_coeff = utils.match_size(M3.coeff,M4.coeff)
+    else:
+        new_M3_coeff, new_M4_coeff = M3.coeff, M4.coeff
+
+    assert np.allclose(new_M3_coeff, new_M4_coeff)
+
+def test_evaluate():
+    # Evaluate .5xyz + 2x + y + z at (4,2,1)
+    poly = MultiPower(np.array([[[0,1,0],
+                                [1,0,0],
+                                [0,0,0]],
+                                [[2,0,0],
+                                [0,.5,0],
+                                [0,0,0]]]))
+
+    assert(poly((4,2,1)) == 15)
+
+def test_evaluate2():
+    # Evaluate -.5x^2y + 2xy^2 - 3z^2 + yz at (7.4,2.33,.25)
+    poly = MultiPower(np.array([[[0,0,-3],
+                                [0,1,0],
+                                [0,0,0]],
+                                [[0,0,0],
+                                [0,0,0],
+                                [2,0,0]],
+                                [[0,0,0],
+                                [-.5,0,0],
+                                [0,0,0]]]))
+
+    assert(np.isclose(poly((7.4, 2.33, .25)), 16.94732))
